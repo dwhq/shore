d60eaa5aca6da18be100157f411dd05f65d64805
1581176417 <1581176417@qq.com>
2018-09-25 15:16:22 +0800

    管理后台 - 商品列表

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -8,6 +8,7 @@ class ProductSku extends Model
 {
     //
     protected $fillable = ['title', 'description', 'price', 'stock'];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
