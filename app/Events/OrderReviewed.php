51ee85b99bd7edecefb96e8d51b42e2be1fb7ddc
1581176417 <1581176417@qq.com>
2018-10-11 15:32:13 +0800

    评价商品

---
+++ app/Events/OrderReviewed.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+
+class OrderReviewed
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    protected $order;
+
+    public function __construct(Order $order)
+    {
+        //
+        $this->order = $order;
+    }
+
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
