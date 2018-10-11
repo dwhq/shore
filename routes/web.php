b060007d8004b1719d720f7b6e551076885b1c04
1581176417 <1581176417@qq.com>
2018-10-11 11:41:18 +0800

    确认收货

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,8 @@ Route::prefix('/')->middleware('auth')->group(function () {
         Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
         //支付完成跳转
         Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+        //确认收货
+        Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');
     });
 });
 //支付宝回调
